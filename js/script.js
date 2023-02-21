f43377e4153911c616436fe5821ac3bbf2cbe77c
wein66 <40013461+wein66@users.noreply.github.com>
2023-02-21 12:11:28 +0900

    공지사항 추가

--- js/script.js
+++ js/script.js
@@ -25,4 +25,15 @@ $(function(){
           $('.slidein').css('left', 0);
        });
     }
+
+    //tab
+    $('.tab a').click(function(e){
+       e.preventDefault();
+       $('.tab a').removeClass('active'); //탭의 모든 active를 지운다.
+       $(this).addClass('active'); //누른탭에 active를 추가한다.
+       const myid = $(this).data("idx"); //누른탭에 data-idx값을 가져온다.
+       $('.tab-content>div').removeClass('active'); //tab-content의 모든 active를 지운다.
+       $("#"+myid).addClass('active'); //가져온 data-idx 값과 같은 tab-content에 active를 추가한다.
+    }) 
+
 });  //jquery
